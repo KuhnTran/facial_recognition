fd91f3da6773b40034171db24e557b9abddc1398
KuhnTran <KuhnTran@gmail.com>
2020-06-04 20:43:58 -0700

    debugging loading not showing up deployment

--- src/components/General/LoadingCircle.js
+++ src/components/General/LoadingCircle.js
@@ -4,13 +4,19 @@ import './LoadingCircle.css';
 
 const LoadingCircle = (props) =>
 {
-    return (          
+    return (  
+        <div className='pa5 ma4'>
+        <h1 className="flex center">TRYING TO LOAD HERE DO NOT BOTHER OURNVS</h1>
+        </div>
+    /*        
     <div className="flex center loadingio-spinner-eclipse-jgbwib07f6j">
         <div className="ldio-ysbe80p474">
             <div>
             </div>
         </div>
      </div>);
+     */
+    )
 }
 
 export default LoadingCircle;
